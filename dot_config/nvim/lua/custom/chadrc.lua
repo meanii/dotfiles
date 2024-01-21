676c6d8e17a4a9bb84946fc1085653203a02b36f
anil <anilchauhanxda@gmail.com>
2024-01-21 12:03:42 +0530

    nvim configs

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { theme = 'ayu_dark' }
+M.plugins = "custom.plugins"
+
+return M
---
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,62 @@
+local plugins = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- backend
+        "go",
+        "elixir",
+        "rust",
+
+        -- web devel
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        -- "vue", "svelte",
+
+       -- low level
+        "c",
+        "zig"
+      },
+    },
+  },
+
+  -- Override plugin definition options 
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+        "gopls",
+      }
+    }
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end
+  }
+
+}
+return plugins
