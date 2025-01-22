253ee77dd0233eb2b9741ea94b8fbfbf03d309a4
Anil Chauhan <anilchauhanxda@gmail.com>
2025-01-23 05:11:36 +0530

    adobting window sizing in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,53 +1,71 @@
 local wezterm = require("wezterm")
 
 local config = {
-	-- color_scheme = 'termnial.sexy',
+	-- Appearance
 	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font_size = 16.0,
 	font = wezterm.font("JetBrains Mono"),
-	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
+	font_size = 16.0,
+	enable_tab_bar = false,
 
-	window_background_image = "/Users/anil/.config/wezterm/backgrounds/blue-chains.png",
+	-- Background Customization
+	window_background_image = wezterm.config_dir .. "/backgrounds/blue-chains.png",
 	window_background_image_hsb = {
-		brightness = 0.01,
+		brightness = 0.05, -- Slightly brighter for better visibility
 		hue = 1.0,
 		saturation = 0.5,
 	},
-	window_background_opacity = 0.92,
-	-- window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
+	window_background_opacity = 0.9,
+	macos_window_background_blur = 30,
+
+	-- Window Properties
+	initial_rows = 35, -- Adjust window height
+	initial_cols = 120, -- Adjust window width
 	window_decorations = "RESIZE",
+	adjust_window_size_when_changing_font_size = false,
+
+	-- Key Bindings
 	keys = {
+		-- Toggle Fullscreen
 		{
 			key = "f",
 			mods = "CTRL",
 			action = wezterm.action.ToggleFullScreen,
 		},
+		-- Clear Scrollback
 		{
 			key = "L",
 			mods = "CTRL",
 			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
 		},
-		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+		-- Navigation with Option Key
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-		-- Make Option-Right equivalent to Alt-f; forward-word
 		{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
-		-- Make Option-Home equivalent to Ctrl-A; move to start of line
 		{ key = "Home", mods = "OPT", action = wezterm.action({ SendString = "\x01" }) },
-		-- Make Option-End equivalent to Ctrl-E; move to end of line
 		{ key = "End", mods = "OPT", action = wezterm.action({ SendString = "\x05" }) },
 	},
+
+	-- Mouse Bindings
 	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
+		-- Ctrl-click to open link
 		{
 			event = { Up = { streak = 1, button = "Left" } },
 			mods = "CTRL",
 			action = wezterm.action.OpenLinkAtMouseCursor,
 		},
 	},
+
+	-- Center Window on Launch (macOS/Linux)
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	},
 }
+
+-- Center window for platforms supporting it
+if wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple:find("linux") then
+	config.initial_position = { x = "center", y = "center" }
+end
 
 return config
